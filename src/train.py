52d9ac0a082b5ad7df5d9eef0d2efdccbf6223ed
jkoicata <jesicajkoicata@gmail.com>
2026-08-05 21:09:52 -0500

    Función Entrenar Modelo
    Se define función de entrenamiento de modelo y se invoca la misma

--- src/train.py
+++ src/train.py
@@ -3,14 +3,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error 
 
 
-# entrenar el modelo
-X = df[["temperatura_c", "precio_promedio", "es_finde"]]
-y = df["ventas_unidades"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def entrenar_modelo():
+ 
+    # entrenar el modelo
+    X = df[["temperatura_c", "precio_promedio", "es_finde"]]
+    y = df["ventas_unidades"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+ 
+    modelo = LinearRegression()
+    modelo.fit(X_train, y_train)
+ 
+    pred = modelo.predict(X_test)
+    print("MAE:", mean_absolute_error(y_test, pred))
+    print("listo!!")
+ 
+    return modelo
 
-modelo = LinearRegression()
-modelo.fit(X_train, y_train)
-
-pred = modelo.predict(X_test)
-print("MAE:", mean_absolute_error(y_test, pred))
-print("listo!!")
+if __name__ == "__main__":
+    entrenar_modelo()
